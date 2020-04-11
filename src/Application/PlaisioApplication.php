6fa889e430ac4cef233aafc0953435decceb3e9d
Paul Water <p.r.water@setbased.nl>
2020-04-11 13:49:47 +0200

    Release: 0.2.7

--- src/Application/PlaisioApplication.php
+++ src/Application/PlaisioApplication.php
@@ -17,7 +17,7 @@ class PlaisioApplication extends Application
    */
   public function __construct()
   {
-    parent::__construct('Plaisio', '0.2.6');
+    parent::__construct('Plaisio', '0.2.7');
 
     $this->setCommandLoader(new CommandLoader(new BufferIO()));
   }
