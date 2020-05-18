fd82b0c6bdc5cfa377fcb7d35bf5e637facd382e
Paul Water <p.r.water@setbased.nl>
2020-05-18 07:02:05 +0200

    Add trait for commands the require access to the kernel.

---
+++ src/Command/PlaisioKernelCommand.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace Plaisio\Console\Command;
+
+use Plaisio\PlaisioKernel;
+
+/**
+ * Trait for commands that require access to the kernel of PhpPlaisio.
+ */
+trait PlaisioKernelCommand
+{
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * The kernel of PhpPlaisio.
+   *
+   * @var PlaisioKernel
+   */
+  protected $nub;
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Sets kernel of PhpPlaisio.
+   *
+   * @param PlaisioKernel $nub The kernel of PhpPlaisio.
+   */
+  public function setPlaisioKernel(PlaisioKernel $nub)
+  {
+    $this->nub = $nub;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+}
+
+//----------------------------------------------------------------------------------------------------------------------
